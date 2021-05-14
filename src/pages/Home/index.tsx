ffe0fefbc843e8a7411432e41297810257653319
diver90 <mihail.zavalniuk@gmail.com>
2021-05-14 19:02:59 +0300

    homework-3 start

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import s from './Home.module.scss';
+
+const HomePage = () => {
+    return (
+        <div className={s.root}>
+
+        </div>
+    );
+};
+
+export default HomePage;
