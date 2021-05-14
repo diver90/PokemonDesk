81f2b4a149ce7dcdc10585bcae733cab5ae6910c
diver90 <mihail.zavalniuk@gmail.com>
2021-05-15 02:07:15 +0300

    homework-3 in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import cn from 'classnames';
-import s from './App.module.scss';
-
+import HomePage from './pages/Home';
 
 const App = () => {
-    return (
-        <div className={cn(s.header, 'color')}>
-            This is App component!
-        </div>
-    )
-}
+  return <HomePage/>;
+};
 
 export default App;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-
+import Header from '../../components/Header';
+import Button from "../../components/Button";
 import s from './Home.module.scss';
 
 const HomePage = () => {
-    return (
-        <div className={s.root}>
+  return (
+    <div className={s.root}>
+      <Header />
 
-        </div>
-    );
+          <div >
+            <h1>
+                <b>Find</b> all your favorite <b>Pokemon</b>
+            </h1>
+              <p>You can choose</p>
+              <Button onClick={()=>console.log('Click Button')}>
+                  See Pokemons
+              </Button>
+          </div>
+    </div>
+  );
 };
 
 export default HomePage;
