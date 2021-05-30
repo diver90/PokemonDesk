5d3d67659bd27009819f1cde8f2a869cbea27419
diver90 <mihail.zavalniuk@gmail.com>
2021-05-30 22:52:04 +0300

    homework-7 in progress

--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -1,13 +1,25 @@
 import config, {ConfigEndpointKeyType} from '../config';
 
-function getUrlWithParamsConfig(endpointCofig: string, query: object) {
+function getUrlWithParamsConfig(endpointCofig: string, query: any) {
     let url = {
         ...config.client.server,
         ...config.client.endpoint[endpointCofig as ConfigEndpointKeyType].uri,
-        query: {
-            ...query
-        }
+        query: {}
     };
+
+    const pathname = Object.keys(query).reduce((acc, value)=> {
+        if (acc.indexOf(`{${value}}`) !== -1){
+            const result = acc.replace(`{${value}}`, query[value]);
+            delete query[value];
+            return result;
+        }
+        return acc;
+    }, url.pathname);
+
+    url.pathname = pathname;
+    url.query = {
+        ...query
+    }
 
     return url;
 }
